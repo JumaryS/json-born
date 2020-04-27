dfeba9399e01da187e54044e984fbe3de14a67e1
JumarySanchez <sanchezjumary@gmail.com>
2020-04-27 15:13:09 -0400

    needs some work

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-const fs = require('fs')
+const fs = require('fs'){
+
+//NEED AYUDA PLEASE//
+
+// fs.readFile('./tests.json', function(error, data) {
+//     if (error) {
+//       throw error;
+//     }
+  
+//     const actualObj = JSON.parse(data);
+//     console.log(actualObj)
+//   })
+
+if(process.argv[2] === 'GET'){
+    fs.readFile('./users.json', function(error, data) {
+    if (error) {
+        throw error;}
+    // } else if ()
+
+const actualObj = JSON.parse(data);
+    console.log(actualObj)
